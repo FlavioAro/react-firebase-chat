0f6e8a406712be5909bc504496baf2e044441541
Flavio Aro <35077695+FlavioAro@users.noreply.github.com>
2021-12-28 10:23:54 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCHkKiNnNutO",
-    authDomain: "196f1",
-    projectId: "196f1",
-    storageBucket: "appspot.com",
-    messagingSenderId: "8931",
-    appId: "eafb1c876eb4c9d6",
-    measurementId: "3568"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 })
 
 const db = firebaseApp.firestore()
